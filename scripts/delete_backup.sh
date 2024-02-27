985d3438892ac54cc48f242eb5015376c964e583
hungmv <hungmv@vinahost.vn>
2024-02-27 09:00:31 +0700

    modified:   scripts/delete_backup.sh

--- scripts/delete_backup.sh
+++ scripts/delete_backup.sh
@@ -3,27 +3,9 @@
 # Đường dẫn đến thư mục chứa các bản backup
 backup_folder="/mnt/backup_truenas/system_backups"
 
-# Số lượng bản backup tối đa bạn muốn giữ lại
-max_backups=10
+max_backups=7
 
-# Đếm số lượng bản backup hiện tại trong thư mục
-backup_count=$(ls -1 "$backup_folder" | wc -l)
 
-# Nếu số lượng bản backup lớn hơn max_backups, thực hiện xóa các bản backup cũ
-if [ "$backup_count" -gt "$max_backups" ]; then
-    # Tính số lượng bản backup cần xoá
-    backups_to_delete=$((backup_count - max_backups))
+find $backup_folder -type d -name "[0-9][0-9]-[0-9][0-9]-[0-9][0-9]" ! -name "*-01-*" -mtime +$max_backups -exec rm -rf {} \; -prune
 
-    # Lấy danh sách các bản backup từ cũ đến mới
-    backups=$(ls -1t "$backup_folder")
-
-    # Xoá các bản backup cũ
-    for ((i = 1; i <= backups_to_delete; i++)); do
-        index=$((max_backups + i))
-        backup_to_delete=$(echo "$backups" | sed -n "${index}p")
-        rm -rf "$backup_folder/$backup_to_delete"
-        echo "Đã xoá bản backup cũ: $backup_to_delete"
-    done
-else
-    echo "Số lượng bản backup hiện tại không vượt quá $max_backups, không cần xoá."
-fi
+find $backup_folder -type d -name "[0-9][0-9]-[0-9][0-9]-[0-9][0-9]" \( -name "*-01-*" \) -mtime +92 -exec rm -rf {} \; -prune
