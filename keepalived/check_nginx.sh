c0b54393b12c31ef7922c85af0808489cc5dd721
mvh97hg <mvh97hg@gmail.com>
2025-01-04 01:45:48 +0700

    new file:   keepalived/check_nginx.sh 	new file:   keepalived/keepalived.conf 	new file:   keepalived/nginx_stub_status.conf

---
+++ keepalived/check_nginx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Kiểm tra trạng thái của Nginx bằng cách gửi một yêu cầu HTTP
+curl -s http://localhost/nginx_status | grep -q "Active connections"  # Đảm bảo rằng Nginx trả về trạng thái hoạt động
+
+if [ $? -eq 0 ]; then
+    exit 0  # Nếu có phản hồi hợp lệ, trả về 0 (tốt)
+else
+    exit 1  # Nếu không có phản hồi hoặc có lỗi, trả về 1 (lỗi)
+fi
