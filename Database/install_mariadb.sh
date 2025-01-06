270eea8438779147cf919ca2af8b44ddf1e9e538
Hungmv <88535301+mvh97hg@users.noreply.github.com>
2025-01-06 10:46:35 +0700

    add zabbix config

--- Database/install_mariadb.sh
+++ Database/install_mariadb.sh
@@ -3,15 +3,8 @@
 sudo apt update && sudo apt upgrade
 sudo apt -y install software-properties-common
 
-sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version=10.10
 
-sudo add-apt-repository 'deb [arch=amd64] http://mariadb.mirror.globo.tech/repo/10.5/ubuntu $(lsb_release -cs) main'
+sudo apt install mariadb-server -y
 
-
-sudo apt update
-sudo apt install mariadb-server
-
-
-#
-
-mysql_secure_installation 
+#mysql_secure_installation 
