0caf293e51ef3fdcbac63668ca43072fc2df4d2a
Hungmv <88535301+mvh97hg@users.noreply.github.com>
2025-03-21 17:06:34 +0700

    a

--- csf/csfpost.sh
+++ csf/csfpost.sh
@@ -5,4 +5,5 @@ iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 # Allow docker0 interface
 iptables -I INPUT -i docker0 -j ACCEPT
+iptables -I OUTPUT -o docker0 -j ACCEPT
 iptables -I FORWARD -i docker0 -j ACCEPT
